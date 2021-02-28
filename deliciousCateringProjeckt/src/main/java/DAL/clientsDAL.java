e937838e1fe1c89d85e7e29fef4b3dd82cc30243
Picaso420 <marinus.xb@gmail.com>
2021-02-28 16:52:43 +0200

    Added the DAL code.

--- deliciousCateringProjeckt/src/main/java/DAL/clientsDAL.java
+++ deliciousCateringProjeckt/src/main/java/DAL/clientsDAL.java
@@ -18,5 +18,20 @@ import java.text.ParseException;
  * @author Picaso
  */
 public class clientsDAL {
-    
+    public List<clients> GetClients() throws FileNotFoundException, ParseException
+    {
+        List<clients> clientsList = new ArrayList<>();	
+        File file = new File("C:\\Users\\user-pc\\Documents\\clients.txt");
+	Scanner sc = new Scanner(file);
+	
+        while(sc.hasNextLine())
+	{         
+            String[] line = sc.nextLine().split(",");
+            //SimpleDateFormat dt = new SimpleDateFormat("MM/dd/yyyy");
+            
+            clientsList.add(new clients(line[0], line[1], line[2], Integer.parseInt(line[3]), Integer.parseInt(line[4]), line[5], line[6], line[7], Double.parseDouble(line[8])));
+	}
+        
+	return clientsList;
+    }
 }
--- deliciousCateringProjeckt/src/main/java/businessLayer/clients.java
+++ deliciousCateringProjeckt/src/main/java/businessLayer/clients.java
@@ -17,5 +17,125 @@ import java.time.ZoneId;
  * @author Picaso
  */
 public class clients {
-   
+    //Variable declarations.
+    private String address;
+    private String eventType;
+    private Date date;
+    private int time;
+    private int amountOfPeople;
+    private String theme;
+    private String decorations;
+    private String foodSelection;
+    private Double price;
+    
+    //Set variables.    
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    
+    public void setEventType(String eventType)
+    {
+        this.eventType = eventType;
+    }
+    
+    public void setDate(Date date)
+    {
+        this.date = date;
+    }
+    
+    public void setTime(int time)
+    {
+        this.time = time;
+    }
+    
+    public void setAmountOfPeople(int amountOfPeople)
+    {
+        this.amountOfPeople = amountOfPeople;
+    }
+    
+    public void setTheme(String theme)
+    {
+        this.theme = theme; 
+    }
+    
+    public void setDecorations(String decorations)
+    {
+        this.decorations = decorations;
+    }
+    
+    public void setFoodSelections(String foodSelection)
+    {
+        this.foodSelection = foodSelection;
+    }
+    
+    public void setPrice(Double price)
+    {
+        this.price = price;
+    }
+    
+    //Get variables.    
+    public String getAddress()
+    {
+        return address;
+    }
+    
+    public String getEventType()
+    {
+        return eventType;
+    }
+    
+    public Date getDate()
+    {
+        return date;
+    }
+    
+    public int getTime()
+    {
+        return time;
+    }
+    
+    public int getAmountOfPeople()
+    {
+        return amountOfPeople;
+    }
+    
+    public String getTheme()
+    {
+        return theme;
+    }
+    
+    public String getDecorations()
+    {
+        return decorations;
+    }
+    
+    public String getFoodSelections()
+    {
+        return foodSelection;
+    }
+    
+    public Double getPrice()
+    {
+        return price;
+    }
+    
+    //Create clients method.
+    public clients()
+    {
+        
+    }
+    
+    public clients(String address, String eventType, String date, int time, int amountOfPeople, String theme, String decorations, String foodSelection, Double price) throws ParseException
+    {
+        this.address = address;
+        this.eventType = eventType;
+        this.date = new SimpleDateFormat("MM/dd/yyyy").parse(date);
+        this.time = time;
+        this.amountOfPeople = amountOfPeople;
+        this.theme = theme;
+        this.decorations = decorations;
+        this.foodSelection = foodSelection;
+        this.price = price;       
+    }
 }
