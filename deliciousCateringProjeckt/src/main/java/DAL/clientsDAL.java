c6a1889140471b1e5d2d8e01e007fe64df13a420
Picaso420 <79565088+Picaso420@users.noreply.github.com>
2021-02-28 16:46:43 +0200

    Add files via upload
    Base for the project.

---
+++ deliciousCateringProjeckt/src/main/java/DAL/clientsDAL.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import businessLayer.*;
+import java.text.ParseException;
+
+/**
+ *
+ * @author Picaso
+ */
+public class clientsDAL {
+    
+}
---
+++ deliciousCateringProjeckt/src/main/java/businessLayer/clients.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLayer;
+
+import java.io.*;
+import java.util.*;
+import DAL.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+/**
+ *
+ * @author Picaso
+ */
+public class clients {
+   
+}
